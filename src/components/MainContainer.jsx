2eff46e1dc8ee3e9f446cb8e955a254748d6d575
Sakshi Tiwari <sakshitiwarim5@gmail.com>
2025-03-01 01:35:02 +0530

    trailer added

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -11,7 +11,7 @@ const MainContainer = () => {
   }
 
   const mainMovie = movies[0];
-  console.log(mainMovie);
+//   console.log(mainMovie);
   const { original_title, overview } = mainMovie;
 
   return (
