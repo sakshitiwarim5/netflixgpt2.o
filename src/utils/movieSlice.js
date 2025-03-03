d4c70b8265995855a397639747bbb86ba34c86e6
Sakshi Tiwari <sakshitiwarim5@gmail.com>
2025-03-03 23:30:02 +0530

    popularMovies

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -3,21 +3,25 @@ import { createSlice } from "@reduxjs/toolkit";
 const movieSlice = createSlice({
   name: "movie",
   initialState: {
-    nowPlayingMovies: [], 
-    trailerVideos:[],
+    nowPlayingMovies: [],
+    trailerVideos: [],
     // ✅ Ensure it's an empty array
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
-    //   console.log("Dispatching Movies to Redux:", action.payload); // ✅ Log when movies are added
+      //   console.log("Dispatching Movies to Redux:", action.payload); // ✅ Log when movies are added
       state.nowPlayingMovies = action.payload || [];
     },
+    addPopularMovies: (state, action) => {
+      //   console.log("Dispatching Movies to Redux:", action.payload); // ✅ Log when movies are added
+      state.nowPopularMovies = action.payload || [];
+    },
     addTrailerVideos: (state, action) => {
-        state.trailerVideos = action.payload || [];
-    }
+      state.trailerVideos = action.payload || [];
+    },
   },
 });
 
-export const { addNowPlayingMovies ,addTrailerVideos} = movieSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideos,addPopularMovies } = movieSlice.actions;
 
 export default movieSlice.reducer;
